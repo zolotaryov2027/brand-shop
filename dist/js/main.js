add7afee579b2cb0f259e208a75d97956f658329
Anton Zolotaryov <anton.zolotaryov2020@gmail.com>
2022-04-29 13:30:49 +0300

    navigation desktop create

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,16 @@
 const menuBurger = document.querySelector('.menu-burger');
 const nav = document.querySelector('.navigation');
 const navigationClose = nav.querySelector('.navigation__close');
+const intro = document.querySelector('.intro');
 
 
 
 menuBurger.addEventListener('click', function(){
   nav.classList.add('navigation--active');
+  intro.classList.add('intro--overlay');
 })
 
 navigationClose.addEventListener('click', function(){
   nav.classList.remove('navigation--active');
+  intro.classList.remove('intro--overlay');
 })
